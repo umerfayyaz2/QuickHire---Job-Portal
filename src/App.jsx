67f04a300cdede358e3e75e4211354b4b8b2b32d
umerfayyaz2 <ufayyaz19@gmail.com>
2025-05-01 02:38:50 +0500

    -Installed Shadcn. -Installed Design Components. -Defined Pages. -Defined Page Routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
+import AppLayout from './layout/app-layout';
+import LandingPage from './pages/landing';
+import Onboarding from './pages/onboarding';
+import JobListing from './pages/job-listing';
+import JobPage from './pages/job';
+import PostJob from './pages/post-job';
+import SavedJobs from './pages/saved-job';
+import MyJobs from './pages/my-jobs';
+
+const router = createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children:[
+      {
+        path:'/',
+        element:<LandingPage/>
+      },{
+        path:'/onboarding',
+        element:<Onboarding/>
+      }
+      ,{
+        path:'/jobs',
+        element:<JobListing/>
+      },{
+        path:'/job/:id',
+        element:<JobPage/>
+      },{
+        path:'/post-job',
+        element:<PostJob/>
+      },{
+        path:'/saved-job',
+        element:<SavedJobs/>
+      },{
+        path:'/my-jobs',
+        element:<MyJobs/>
+      }
+    ]
+  }
+])
 
 function App() {
-
   return (
-    <>
-      <div>Hello React</div>
-      
-    </>
-  )
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App
