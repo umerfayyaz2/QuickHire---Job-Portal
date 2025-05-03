ddee9ca850eff020a9351f6bb3536d9b05ec361c
umerfayyaz2 <ufayyaz19@gmail.com>
2025-05-04 01:51:09 +0500

    -Added .env API's for supabase and clerk -installed clerk and supabase -integrated user auth and registartion. -set up login -changed theme for clerk auth into shadesOfPurple

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 const Header = () => {
   return (
@@ -10,7 +11,17 @@ const Header = () => {
     <img src="/public/logo.png" alt="website logo" className='h-20' />
     </Link>
 
-    <Button variant="outline">Login</Button>
+    {/* <Button variant="outline">Login</Button> */}
+
+    {/* If the user is sign-out they will see the sign in button */}
+    <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    {/* If the user is sign-in they will see the sign-in button */}
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
     </nav>
    </>
   )
